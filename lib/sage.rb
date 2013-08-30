4bc80412cdf6883d66d81d86dfed8a6f13bae729
Shou Ya <shouyatf@gmail.com>
2013-08-31 04:37:11 +0800

    REPL!

---
+++ lib/sage.rb
@@ -0,0 +1,7 @@
+
+
+require File.join(File.dirname(__FILE__), 'sage/parser')
+require File.join(File.dirname(__FILE__), 'sage/identifier')
+require File.join(File.dirname(__FILE__), 'sage/lambda')
+require File.join(File.dirname(__FILE__), 'sage/application')
+
--- lib/sage/parser.rb
+++ lib/sage/parser.rb
@@ -1,28 +1,10 @@
 require 'treetop'
 
 require File.join(File.dirname(__FILE__), 'parser_nodes')
-
-module Sage
-  class Parser
-    Treetop.load(File.join(File.dirname(__FILE__),
-                           'parser.treetop'))
-    @@parser = SageParser.new
-
-    def self.parse(data)
-      tree = @@parser.parse(data)
-
-      if tree.nil?
-        p @@parser
-        raise Exception, "Parse error at offset: #{@@parser.index}"
-      end
-
-      return tree
-    end
-  end
-end
+Treetop.load(File.join(File.dirname(__FILE__), 'parser.treetop'))
 
 if __FILE__ == $0
   require 'ap'
-  ap Sage::Parser.parse('\m.e b a').parse.to_s
+  ap Sage::Parser.parse('\m.').parse.to_s
 end
 
